5db452c2d8bf7f95a4e55ae9a9ebbc5e55eabaab
Stefano.In91 <ste.ince91@gmail.com>
2022-10-27 14:53:30 +0200

    adds js final

--- js/app.js
+++ js/app.js
@@ -1,2 +1,21 @@
 "use strict";
 
+// Data una lista della spesa
+const groceryList = [
+   "Pane",
+   "Acqua",
+   "Proteine",
+   "CoseACaso",
+   "Caramelle",
+   "Cinghiale"
+];
+const listLength = groceryList.length;
+// Lista su cui stampare
+const ul = document.getElementById("grocery-list");
+// Ciclo di stampa
+let i = 0;
+while (i < listLength) {
+   const li = document.createElement("li");
+   ul.append(li);
+   li.innerHTML = groceryList[i++];
+}
